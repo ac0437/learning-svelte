40f981a47785e181b735152d5aa3a95a11811e8a
Adonis Cruz <adonisacruz0@gmail.com>
2023-07-22 13:37:09 -0400

    creating a counter in svelte

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<!-- @format -->
+<!-- @format -->
+<script>
+  // @ts-nocheck
+
+  let count = 0;
+
+  function increment() {
+    count++;
+    console.log(count);
+  }
+
+  function decrement() {
+    count--;
+    console.log(count);
+  }
+</script>
+
+<button on:click={increment}>Increment</button>
+<button on:click={decrement}>Decrement</button>
+<span>{count}</span>
+
+<style>
+</style>
