eb4c60ce60c69a7dabca46f861c62c3ac20135d7
Adonis Cruz <adonisacruz0@gmail.com>
2023-07-23 15:01:48 -0400

    Data binding can be done with {} If data has the same name as attribute we can use a short hand i.e. {src} is short hand for src="{src}"

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -4,21 +4,54 @@
   // @ts-nocheck
 
   let count = 0;
+  const src = 'https://svelte.dev/svelte-logo-horizontal.svg';
 
   function increment() {
     count++;
-    console.log(count);
   }
 
   function decrement() {
     count--;
-    console.log(count);
   }
 </script>
 
-<button on:click={increment}>Increment</button>
-<button on:click={decrement}>Decrement</button>
-<span>{count}</span>
+<nav>
+  <img {src} alt="Svelte-logo" />
+  <span>Counter</span>
+</nav>
+<section>
+  <div class="count-container">
+    <span>{count}</span>
+  </div>
+  <button on:click={increment}>Increment</button>
+  <button on:click={decrement}>Decrement</button>
+</section>
 
 <style>
+  nav {
+    display: flex;
+    align-items: center;
+  }
+  img {
+    width: 200px;
+  }
+  span {
+    font-size: 40px;
+  }
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .count-container {
+    text-align: center;
+  }
+  section button {
+    width: 100px;
+    padding: 4px 8px;
+  }
+  section button {
+    margin-top: 8px;
+  }
 </style>
