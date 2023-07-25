5bde03f7811ace24b76906ea67cbd5f5484d633f
Adonis Cruz <adonisacruz0@gmail.com>
2023-07-25 07:09:14 -0400

    learning about reactive declarations and statements; creating a demo to apply a reactive declaration

---
+++ input-prompt/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<!-- @format -->
+<script>
+  /**
+   * @type {string}
+   */
+  let firstname;
+  /**
+   * @type {string}
+   */
+  let lastname;
+  $: fullname = `Hello, ${firstname} ${lastname}`;
+
+  // @ts-ignore
+  function setFirstName(event) {
+    firstname = event.target.value;
+  }
+
+  // @ts-ignore
+  function setLastName(event) {
+    lastname = event.target.value;
+  }
+</script>
+
+<input on:change={setFirstName} type="text" placeholder="enter firstname" />
+<input on:change={setLastName} type="text" placeholder="enter lastname" />
+<span>Your name is: {fullname} </span>
